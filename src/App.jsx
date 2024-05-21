08b393f3f15468e2c4311fa1fc7e24192bfe2d8b
Osirus Jack <methanblun@gmail.com>
2024-05-21 12:58:56 +0000

    diplay dashbord on UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import Todolist from "./components/toDoList";
 function App() {
   return (
     <>
-   <div className=" flex-row">
-   <div className=" bg-red-500 h-14"></div>
+      <div className="flex flex-row">
+        <div className=" bg-red-300 h-56 w-24"></div>
 
-   </div>
-      <div className=" bg-slate-400 flex flex-col  h-full">
-        
-        <div className=" bg-orange-300 lg:h-32 h-40">
-          <Quote />
-        </div>
+        <div className=" bg-slate-400 flex flex-col  h-full">
+          <div className=" bg-orange-300 lg:h-32 h-40">
+            <Quote />
+          </div>
 
-        <div className=" bg-slate-500  h-2/6">
-          <Todolist />
+          <div className=" bg-slate-500  h-2/6">
+            <Todolist />
+          </div>
+          <div></div>
         </div>
-        <div></div>
       </div>
     </>
   );
