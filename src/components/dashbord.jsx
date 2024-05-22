dc2b1bb3f0c19e7f850b8b71c17a2ba123776a16
Osirus Jack <methanblun@gmail.com>
2024-05-22 15:24:11 +0000

    dashbord design

--- src/components/dashbord.jsx
+++ src/components/dashbord.jsx
@@ -1,36 +1,42 @@
-
-
-
-
- function Dashbord (){
-
-
-  return(
-  <div className="  bg-slate-400  h-full  m-4  shadow-2xl  w-40">
-    <ul className="space-y-16 h-10 w-6">
+function Dashbord() {
+  return (
+    <div className="    pt-10 h-full  m-4      shadow-2xl  w-40">
+      <ul className="space-y-16 h-10 w-6">
         <li>
-          <a href="#" className="block p-2 bg-blue-900 rounded hover:bg-red-200">
-            Dashboard
+          <a
+            href="#"
+            className="block p-2  bg-sky-600  w-36  rounded text-center hover:bg-sky-700"
+          >
+            something
           </a>
         </li>
         <li>
-          <a href="#" className="block p-2 bg-blue-100 rounded hover:bg-blue-200">
-            Courses
+          <a
+            href="#"
+            className="block p-2 w-36 bg-cyan-800 rounded text-center hover:bg-cyan-900"
+          >
+            something
           </a>
         </li>
         <li>
-          <a href="#" className="block p-2 bg-blue-100 rounded hover:bg-blue-200">
-            Students
+          <a
+            href="#"
+            className="block p-2 w-36 bg-rose-800 rounded text-center hover:bg-rose-950"
+          >
+            something
           </a>
         </li>
         <li>
-          <a href="#" className="block p-2 bg-blue-100 rounded hover:bg-blue-200">
-            Teachers
+          <a
+            href="#"
+            className="block p-2  w-36 bg-blue-100 rounded  text-center hover:bg-blue-200"
+          >
+            something
           </a>
         </li>
-        </ul>
-    </div>)
+      </ul>
+    </div>
+  );
 }
-
 
 export default Dashbord;
