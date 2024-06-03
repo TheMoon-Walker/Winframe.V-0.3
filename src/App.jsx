8ab9ec136abbe6183dac6a928e0c93d8b370459b
Osirus Jack <methanblun@gmail.com>
2024-06-03 21:35:33 +0000

    finnaly able to switch between homepage towards vitrine page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // app.jsx
 import HomePage from "./components/homePage";
+import Vitrine from "./components/vitrine";
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/vitrine" element={<Vitrine/>}/>
         <Route path="/" element={<HomePage />} />
       </Routes>
     </>
