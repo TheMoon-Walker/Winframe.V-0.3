be8398f80455fa711c3a2fbc9c42a934ce778f3b
Osirus Jack <methanblun@gmail.com>
2024-07-29 15:12:30 +0000

    user will be redirected in a brand new page when author link is clicked

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -24,20 +24,23 @@ function Quote() {
     fetchData();
   }, []);
 
-
-  function handleSearchClick(){
-    console.log(author)
- const   searchUrl =  `https://www.google.com/search?q=${encodeURIComponent(author)}`;
-   window.location.href = searchUrl;
+  function handleSearchClick() {
+    console.log(author);
+    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(
+      author
+    )}`;
+    window.open(searchUrl, '_blank')
   }
-  
 
   return (
     <blockquote className="p-5 h-full flex flex-col bg-slate-800 md:flex-row md:p-9">
       <div className="flex-grow lg:pl-8 text-gray-50">{quote}</div>
-      <a href='#' onClick={handleSearchClick}><cite className="flex justify-end items-center pr-4 text-sky-300">{author}</cite></a>
+      <a href="#" onClick={handleSearchClick}>
+        <cite className="flex justify-end items-center pr-4 text-sky-300">
+          {author}
+        </cite>
+      </a>
     </blockquote>
-
   );
 }
 
