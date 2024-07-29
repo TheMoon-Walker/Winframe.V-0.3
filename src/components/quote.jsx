eacb92923a35931cb5331fdd3986a9180b0225ac
Osirus Jack <methanblun@gmail.com>
2024-07-29 14:53:53 +0000

    extract author value inside handleSearch function

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -30,27 +30,19 @@ function Quote() {
   //   const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(author)}`;
   // }
 
-  function handleSearchClick({author}){
-    return alert('hell is cold',{author})
-    //ce que j'essayait de faire avant d'aller reprendre
-    //la formation javasript : diriger le user vers une page google avec 
-    //la recherche de l'auteur
+  function handleSearchClick(){
+    console.log(author)
 
+ 
   }
+  
 
   return (
     <blockquote className="p-5 h-full flex flex-col bg-slate-800 md:flex-row md:p-9">
       <div className="flex-grow lg:pl-8 text-gray-50">{quote}</div>
       <a href='#' onClick={handleSearchClick}><cite className="flex justify-end items-center pr-4 text-sky-300">{author}</cite></a>
     </blockquote>
-    // <div className="p-5 h-full flex flex-col bg-slate-800 md:flex-row md:p-9">
-    //   <div className="flex-grow lg:pl-8 text-gray-50">
-    //     {quote}
-    //   </div>
-    //   <div className="flex justify-end items-center pr-4 text-sky-300">
-    //     {author}
-    //   </div>
-    // </div>
+
   );
 }
 
