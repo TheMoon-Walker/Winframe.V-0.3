2fc3206e294bf3b7a2a90c964886794335d0c31b
Osirus Jack <methanblun@gmail.com>
2024-08-11 22:46:44 +0000

    fetch all datas and log em

--- src/components/vitrine.jsx
+++ src/components/vitrine.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import supabase from "../config/supabaseClient";
+import { data } from "autoprefixer";
+
 export default function Vitrine() {
+  const [allTasks, setAllTasks] = useState();
+
+  const getAlltasks = async () => {
+    const { data, error } = await supabase.from("toDoList").select("*");
+    if (error) {
+      console.error("Error adding task:", error);
+      return;
+    }
+    setAllTasks(data);
+    console.log(allTasks);
+  };
+
+
+
+
+
   return (
     <div className=" flex flex-col h-screen  bg-sky-900">
-      <div className=" bg-red-400 h-1/2"> Taches a faire</div>
-      <div className=" bg-green-600 h-1/2">Taches Faites</div>
-
-      <div></div>
+      <div className=" bg-red-400 h-1/2">
+        {" "}
+        Taches a faire
+        <button onClick={getAlltasks}>click here</button>
+      </div>
+      <div className=" bg-green-600 h-1/2"></div>
+      <div>
+      </div>
     </div>
   );
 }
--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -16,7 +16,7 @@ export default function ErrorPage() {
       </p>
       <button
         className="mt-6 px-4 py-2  bg-sky-600 text-white rounded hover:bg-blue-700 focus:outline-none"
-        onClick={() => window.location.href = "/"}
+        onClick={() => (window.location.href = "/")}
       >
         Go to Home
       </button>
