3fd346895a070eb347cd24389b00f7414a2c6877
Osirus Jack <methanblun@gmail.com>
2024-08-12 13:20:14 +0000

    ternary operation for diplaying is_done status

--- src/components/vitrine.jsx
+++ src/components/vitrine.jsx
@@ -4,66 +4,28 @@ import { useEffect } from "react";
 
 export default function Vitrine() {
   const [allTasks, setAllTasks] = useState([]);
- 
+
   const getAlltasks = async () => {
     const { data, error } = await supabase.from("toDoList").select("*");
-    
+
     if (error) {
       console.error("Error adding task:", error);
       return;
     }
     setAllTasks(data);
-  
-    allTasks.map(task => console.log(task.id))
 
+    allTasks.map((task) => console.log(task.tas));
   };
 
   return (
     <div className=" flex flex-col h-screen  bg-sky-900">
       <div className="">
-     
         <button onClick={getAlltasks}>click here</button>
-        <div><ul>
-          {/* {allTasks.map(task => <li key={}>{task}</li>)} */}
-          
-          </ul></div>
+        <div>
+              {allTasks.map(task => <li key={task.id}>{task.task_content} <strong>{task.is_done? 'Yes' : 'Non'}</strong> </li>)}
+
+        </div>
       </div>
     </div>
   );
 }
-// import { useState } from "react";
-// import supabase from "../config/supabaseClient";
-
-// export default function Vitrine() {
-//   const [allTasks, setAllTasks] = useState([]);
- 
-//   const getAlltasks = async () => {
-//     const { data, error } = await supabase.from("toDoList").select("*");
-    
-//     if (error) {
-//       console.error("Error fetching tasks:", error);
-//       return;
-//     }
-//     setAllTasks(data);
-//   };
-
-//   return (
-//     <div className="flex flex-col h-screen bg-sky-900">
-//       <div className="bg-red-400 h-1/2">
-//         <button onClick={getAlltasks}>Cliquez ici</button>
-//       </div>
-//       <div className="bg-green-600 h-1/2">
-//         <ul>
-//           {allTasks.map((task) => (
-//             <li key={task.id}>
-//               {/* Affichage des propriétés dynamiques */}
-//               <strong>ID:</strong> {task.id}, 
-//               <strong> Task:</strong> {task.task}, 
-//               <strong> Done:</strong> {task.is_done ? "Yes" : "No"}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
