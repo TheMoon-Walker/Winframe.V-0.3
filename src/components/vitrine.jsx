f23acab0513ea0a9784fdabb506939f34eca748b
Osirus Jack <methanblun@gmail.com>
2024-08-12 13:32:04 +0000

    fetch created at data and display it

--- src/components/vitrine.jsx
+++ src/components/vitrine.jsx
@@ -5,7 +5,8 @@ import { useEffect } from "react";
 export default function Vitrine() {
   const [allTasks, setAllTasks] = useState([]);
 
-  const getAlltasks = async () => {
+  useEffect( ()=>{
+      const getAlltasks = async () => {
     const { data, error } = await supabase.from("toDoList").select("*");
 
     if (error) {
@@ -16,15 +17,18 @@ export default function Vitrine() {
 
     allTasks.map((task) => console.log(task.tas));
   };
+  getAlltasks()
+  },[])
+
 
   return (
     <div className=" flex flex-col h-screen  bg-sky-900">
       <div className="">
-        <button onClick={getAlltasks}>click here</button>
-        <div>
-              {allTasks.map(task => <li key={task.id}>{task.task_content} <strong>{task.is_done? 'Yes' : 'Non'}</strong> </li>)}
+        <ul className="flex flex-col p-8 h-screen   overflow-scroll ">
 
-        </div>
+              {allTasks.map(task => <li key={task.id}>{task.task_content} <strong>{task.is_done? 'Yes' : 'Non'}</strong>  <strong>{task.created_at}</strong></li>)}
+
+        </ul>
       </div>
     </div>
   );
